dfed4eb58337aa4b02f364573c4716aa18151eae
mani-asterdio <mani.dumaru@asterdio.com>
2025-08-02 15:26:22 +0545

    add water plants request, maintenance cycle, check connection status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import { checkSoilMoisture } from "../services/api";
-import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  checkSoilMoisture,
+  checkStatus,
+  fullMaintenanceCycleRequest,
+  waterPlantsRequest,
+} from "../services/api";
+import { BookAIcon, GlassWater, HammerIcon, Loader2 } from "lucide-react";
 import { useSoilMoisture } from "../context/soilMoistureContext";
 import { toast } from "sonner";
 
 const Header = ({ isConnected, onOpenConsole }) => {
   const { setData, setFetchingSoilMoisture, fetchingSoilMoisture } =
     useSoilMoisture();
+
+  const [online, setOnline] = useState(false);
 
   const getSoilMoisture = async () => {
     try {
@@ -23,6 +30,55 @@ const Header = ({ isConnected, onOpenConsole }) => {
     }
   };
 
+  const waterPlants = async () => {
+    try {
+      setFetchingSoilMoisture(true);
+      const res = await waterPlantsRequest();
+
+      setData(res?.readings);
+      toast.success(res?.message || "Moisture data updated");
+    } catch (err) {
+      console.log(err?.message || "Something went wrong");
+      toast.error(err?.message || "Someting went wrong");
+    } finally {
+      setFetchingSoilMoisture(false);
+    }
+  };
+
+  const fullMaintenanceCycle = async () => {
+    try {
+      setFetchingSoilMoisture(true);
+      const res = await fullMaintenanceCycleRequest();
+
+      setData(res?.readings);
+      toast.success(res?.message || "Moisture data updated");
+    } catch (err) {
+      console.log(err?.message || "Something went wrong");
+      toast.error(err?.message || "Someting went wrong");
+    } finally {
+      setFetchingSoilMoisture(false);
+    }
+  };
+
+  const statusRequest = async () => {
+    try {
+      const res = await checkStatus();
+
+      return res;
+    } catch (err) {
+      toast.error(err?.message || "Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const status = await statusRequest();
+      setOnline(status?.online);
+    };
+
+    fetchStatus();
+  }, []);
+
   return (
     <header className="flex jusitfy-center gap-4 bg-[#2E7D32] text-white px-6 py-2 shadow-md">
       <img
@@ -36,27 +92,53 @@ const Header = ({ isConnected, onOpenConsole }) => {
       </div>
       <button
         onClick={() => getSoilMoisture()}
-        className="flex gap-2 items-center justify-center w-[20rem] py-3 px-4 my-auto rounded-lg transition-all bg-green-600 hover:bg-green-700 text-white shadow-lg hover:shadow-green-500/30 cursor-pointer"
+        className="flex rounded-full gap-2 items-center justify-center p-2 my-auto transition-all duration-300 bg-green-600 hover:bg-green-900 text-white shadow-lg hover:shadow-green-500/30 cursor-pointer disabled:pointer-events-none disabled:pointer-none disabled:bg-gray-600"
+        // disabled={fetchingSoilMoisture || !online}
       >
-        {fetchingSoilMoisture && <Loader2 className="animate-spin" />}
-        Read Moisture
+        {fetchingSoilMoisture ? (
+          <Loader2 className="animate-spin" />
+        ) : (
+          <BookAIcon />
+        )}
+      </button>
+      <button
+        onClick={() => waterPlants()}
+        className="flex rounded-full gap-2 items-center justify-center p-2 my-auto transition-all duration-300 bg-green-600 hover:bg-green-900 text-white shadow-lg hover:shadow-green-500/30 cursor-pointer disabled:pointer-events-none disabled:pointer-none disabled:bg-gray-600"
+        disabled={fetchingSoilMoisture || !online}
+      >
+        {fetchingSoilMoisture ? (
+          <Loader2 className="animate-spin" />
+        ) : (
+          <GlassWater />
+        )}
+      </button>
+      <button
+        onClick={() => fullMaintenanceCycle()}
+        className="flex rounded-full gap-2 items-center justify-center p-2 my-auto transition-all duration-300 bg-green-600 hover:bg-green-900 text-white shadow-lg hover:shadow-green-500/30 cursor-pointer disabled:pointer-events-none disabled:pointer-none disabled:bg-gray-600"
+        disabled={fetchingSoilMoisture || !online}
+      >
+        {fetchingSoilMoisture ? (
+          <Loader2 className="animate-spin" />
+        ) : (
+          <HammerIcon />
+        )}
       </button>
       <button
         className={`flex items-center justify-center w-[20rem] py-3 px-4 my-auto rounded-lg transition-all duration-200 ${
-          isConnected
+          online
             ? "bg-green-600 hover:bg-green-700 text-white shadow-lg hover:shadow-green-500/30 cursor-pointer"
             : "bg-gray-600 text-gray-200 cursor-not-allowed"
         }`}
-        disabled={!isConnected}
+        disabled={!online}
         onClick={onOpenConsole}
       >
         <span
           className={`h-3 w-3 rounded-full mr-3 ${
-            isConnected ? "bg-green-300 animate-pulse" : "bg-red-400"
+            online ? "bg-green-300 animate-pulse" : "bg-red-400"
           }`}
         ></span>
         <span className="font-medium">
-          {isConnected ? "Open Console" : "Console Offline"}
+          {online ? "Open Console" : "CNC Offline"}
         </span>
       </button>
     </header>
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,32 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.0.101:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 export async function checkSoilMoisture() {
+  console.log(BASE_URL);
   const res = await axios.post(`${BASE_URL}/sequences/check-all-soil-moisture`);
+
+  return res?.data;
+}
+
+export async function waterPlantsRequest() {
+  const res = await axios.post(`${BASE_URL}/sequences/water-dry-slots`, {
+    threshold: 60,
+  });
+
+  return res?.data;
+}
+
+export async function fullMaintenanceCycleRequest() {
+  const res = await axios.post(`${BASE_URL}/sequences/full-maintenance-cycle`, {
+    watering_threshold: 60,
+  });
+
+  return res?.data;
+}
+
+export async function checkStatus() {
+  const res = await axios.get(`${BASE_URL}/status`);
 
   return res?.data;
 }
